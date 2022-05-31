1c94573547ef55549669353e3717b6bb601a6029
Anjacodes <anja_schmidt7@posteo.de>
2022-05-31 18:04:20 -0500

    Wrap controller in Api module

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,11 +1,13 @@
-class FoodsController < ApplicationController
-  def index
-    @foods = Food.where(category_id: params[:category_id])
-    render json: @foods
-  end
+module Api
+  class FoodsController < ApplicationController
+    def index
+      @foods = Food.where(category_id: params[:category_id])
+      render json: @foods
+    end
 
-  def show
-    @food = Food.find(params[:id])
-    render json: @food
+    def show
+      @food = Food.find(params[:id])
+      render json: @food
+    end
   end
 end
